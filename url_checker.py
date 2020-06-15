588e43263fdaf8872d3b85721a79e92ed8f5880c
marie-brainlabs <marie@brainlabsdigital.com>
2020-06-15 10:46:49 +0100

    Write a function to check status of first 10 google search results

--- url_checker.py
+++ url_checker.py
@@ -1,6 +1,16 @@
 from googlesearch import search
+from urllib3 import PoolManager
 
-query = "Brainlabs Digital"
 
-for search_result in search(query, tld="com", start=0, stop=9, pause=2):
-    print(search_result)
+def check_google_search_results(query: str):
+    search_results = [
+        search_result
+        for search_result in search(query, tld="com", start=0, stop=9, pause=2)
+    ]
+    return [check_url_status(url) for url in search_results]
+
+
+def check_url_status(url: str):
+    http = PoolManager()
+    r = http.request("GET", url)
+    return r.status
